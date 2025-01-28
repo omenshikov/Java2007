de777e958d5d6a0f481eac117f9a5356b02343ca
omenshikov <olegamobil@yandex.ru>
2025-01-28 10:54:12 +0300

    init

---
+++ src/main/java/FirstApp.java
@@ -0,0 +1,20 @@
+public class FirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, java");
+
+        int v1 = 10;
+        int v2 = 15;
+
+
+        if (v1 > v2) {
+            System.out.println("v1 > v2");
+        } else if (v1 < v2) {
+            System.out.println("v1 < v2");
+        } else {
+            System.out.println("v1 <= v2");
+        }
+    }
+
+}
+
